543bcd3fd0c9c3f356e6afed43611a0e6f0c4ac8
aakash_raj <aakash.raj5@gmail.com>
2022-02-13 05:19:42 +0000

    Added logic for all exercises and created test log file

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,101 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  for element in arr
+    sum = sum + element
+  end
+  return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  end
+  x,y = arr.max(2)
+  x+y
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  for i in (0..arr.length-1)
+    for j in (i+1..arr.length-1)
+      if arr[i] + arr[j] == n
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return s =~ /\A[a-z&&[^aeiou]].*/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  reversedString = s.reverse
+  sum = 0
+  invalid = false
+  reversedString.each_char.with_index do |digit,i|
+    if digit =~ /[^10]/i
+      invalid = true
+      break
+    end
+    if digit == '1'
+      sum += 2**i
+    end
+  end
+  if invalid or s.empty?
+    return false
+  end
+  if sum % 4 == 0
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+  def initialize(isbn, price)
+    if isbn.empty?
+      raise ArgumentError
+    end
+    if price <= 0
+      raise ArgumentError
+    end
+    @isbn = isbn
+    @price = price
+  end
+  
+  def isbn=(isbn)
+    if isbn.empty?
+      raise ArgumentError
+    end
+    @isbn = isbn
+  end
+  
+  def price=(price)
+    if price <= 0
+      raise ArgumentError
+    end
+    @price = price
+  end
+  
+  def price_as_string
+    return "$%0.2f" % [price]
+  end
+  
+  attr_reader :isbn
+  attr_reader :price
+  
 end
